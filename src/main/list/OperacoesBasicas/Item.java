7b499493bc24ada356103df932659b35a6f6baeb
Luciana Tokozima <lutokozima@gmail.com>
2023-10-03 16:59:45 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: Luciana Tokozima <lutokozima@gmail.com>

---
+++ src/main/list/OperacoesBasicas/Item.java
@@ -0,0 +1,36 @@
+package main.list.OperacoesBasicas;
+
+public class Item {
+    //atributo
+    private String nome;
+    private double preco;
+    private int quant;
+
+
+    public Item(String nome, double preco, int quant) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quant = quant;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quant=" + quant +
+                '}';
+    }
+}
